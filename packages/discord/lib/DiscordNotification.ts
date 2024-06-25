2a7be4f2175748017839f8f33a74b567f7964014
Mohammad Kermani <mkermani144@gmail.com>
2024-06-25 14:57:51 +0000

    feat: implement Discord notification package

---
+++ packages/discord/lib/DiscordNotification.ts
@@ -0,0 +1,85 @@
+import { AbstractNotification } from '@rosen-bridge/abstract-notification';
+import { WebhookClient } from 'discord.js';
+
+class DiscordNotification extends AbstractNotification {
+  private client: WebhookClient;
+
+  constructor(webhookURL: string) {
+    super();
+    this.client = new WebhookClient({
+      url: webhookURL,
+    });
+  }
+
+  /**
+   * send a string to Discord, trimming all lines
+   * @param message
+   */
+  private sendMessage = (message: string) => {
+    this.client.send({
+      content: message.replace(/^ +| +$/gm, ''), // trim all lines
+    });
+  };
+
+  /**
+   * send an error notification
+   * @param title
+   * @param message
+   */
+  error = async (title: string, message: string) => {
+    this.sendMessage(`
+      ## ❌ ${title}
+      ${message}
+      `);
+  };
+
+  /**
+   * send a warning notification
+   * @param title
+   * @param message
+   */
+  warning = async (title: string, message: string) => {
+    this.sendMessage(`
+      ## ⚠️ ${title}
+      ${message}
+      `);
+  };
+
+  /**
+   * send an info notification
+   * @param title
+   * @param message
+   */
+  info = async (title: string, message: string) => {
+    this.sendMessage(`
+      ## ℹ️ ${title}
+      ${message}
+      `);
+  };
+
+  /**
+   * send a success notification
+   * @param title
+   * @param message
+   */
+  success = async (title: string, message: string) => {
+    this.sendMessage(`
+      ## ✅ ${title}
+      ${message}
+      `);
+  };
+
+  /**
+   * send a raw notification
+   * @param title
+   * @param message
+   */
+  raw = async (title: string, message: string) => {
+    this.sendMessage(`
+      ## ${title}
+      ${message}
+      `);
+  };
+}
+
+export default DiscordNotification;
